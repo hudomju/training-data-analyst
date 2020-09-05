3c3b3b52d03c6c67ed513f0d0e7669af6b2e22b2
Hugo <hudomju@gmail.com>
2020-09-05 11:10:06 +0200

    Update istio installation

--- courses/ahybrid/v1.0/AHYBRID050/scripts/create_gke.sh
+++ courses/ahybrid/v1.0/AHYBRID050/scripts/create_gke.sh
@@ -59,13 +59,21 @@ kpt pkg get https://github.com/GoogleCloudPlatform/anthos-service-mesh-packages.
 
 cd asm
 kpt cfg set asm gcloud.container.cluster ${C1_NAME}
+kpt cfg set asm gcloud.project.environProjectNumber ${PROJECT_NUMBER}
 kpt cfg set asm gcloud.core.project ${PROJECT_ID}
 kpt cfg set asm gcloud.compute.location ${C1_ZONE}
+
+# To configure that all clusters are in the same project
+kpt cfg set asm anthos.servicemesh.profile asm-gcp
 
 gcloud container clusters get-credentials $C1_NAME \
     --zone $C1_ZONE --project $PROJECT_ID
 
+# Install Istio + Enable tracing with Cloud Trace
 istioctl install -f asm/istio/istio-operator.yaml -f $LAB_DIR/training-data-analyst/courses/ahybrid/v1.0/AHYBRID050/scripts/tracing.yaml
+
+# Enable the Anthos Service Mesh UI in Cloud Console
+kubectl apply -f asm/canonical-service/controller.yaml
 
 kubectl wait --for=condition=available --timeout=600s deployment \
   --all -n istio-system
